5a014b7a0f4c13ba285adefc5a28be5ab7ab3bdd
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2024-06-20 12:10:23 +0530

    Add files via upload

---
+++ many_to_many/myapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Song
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['song_name', 'song_duration', 'written_by']
---
+++ many_to_many/myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Song(models.Model):
+    user = models.ManyToManyField(User)
+    song_name = models.CharField(max_length=70)
+    song_duration = models.IntegerField()
+
+    def written_by(self):
+        return ','.join([str(u) for u in self.user.all()])
